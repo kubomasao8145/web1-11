1a677b9d3ee29ffa80d60943ebc34d55857eb307
Takashi Imagire <imagire@gmail.com>
2017-06-19 21:27:49 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,12 +10,12 @@ if(result){
 }
 
 var number=Math.floor(Math.random()*3);
-var answer=parseInt(window.prompt('数当てゲーム  o~2の数字を入力してください'));
 
 var message;
 
 while(1){
-if(answer===number){
+  var answer=parseInt(window.prompt('数当てゲーム  o~2の数字を入力してください'));
+  if(answer===number){
     message='あたり';
     document.getElementById('choice').textContent=message;
     break;
