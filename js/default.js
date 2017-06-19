15f6015c38b165f69032af936bbdab5020289d1c
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-06-19 17:22:12 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -9,7 +9,7 @@ if(result){
   document.getElementById('choice').textContent='キャンセルが押された';
 }
 
-var number=Math.floor(Math.random()*3):
+var number=Math.floor(Math.random()*3);
 var answer=parseInt(window.prompt('数当てゲーム  o~2の数字を入力してください'));
 
 var message;
