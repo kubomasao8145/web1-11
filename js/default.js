9eb49d32624d62b0f9050a55e21f4151482be7a4
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-06-19 17:20:19 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,7 @@
 window.alert('こんにちは世界');
 console.log('こんにちは世界');
+
+document.getElementById('choice').textContent=new Date();
 var result=window.confirm('準備OK?');
 if(result){
   document.getElementById('choice').textContent='Okが押された';
